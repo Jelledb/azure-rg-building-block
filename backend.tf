8d1417e291b6cd93d3e642c0bbc7343ad725500b
Jelle den Burger <Jelledb@users.noreply.github.com>
2025-01-10 17:31:10 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,15 @@
 terraform {
-  backend "azurerm" { 
-    resource_group_name  = "jelles-rg"
-    storage_account_name = "jellestfstorage"
-    container_name       = "tfstates"
-    key                  = "jelles-azure-rg-building-block"
+  backend "s3" {
+    bucket = "buildingblocks-tfstates-p32kj" # Must match what's configured in automation backend
+    key    = "terraform/spotify-bb"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::490004649140:role/LikvidBuildingBlockServiceRole" # Must match what's configured in automation backend
   }
 }
